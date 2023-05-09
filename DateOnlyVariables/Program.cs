2824528d8a9a43a2698b71c3de4bf4c92210b0a5
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-09 00:08:44 -0600

    Add project files.

---
+++ DateOnlyVariables/Program.cs
@@ -0,0 +1,10 @@
+﻿
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse(s: "6/11/1998");
+
+Console.WriteLine(birthday.ToString(format: "MMMM dd, yyyy"));
+
+Console.WriteLine(value: $"Today full format: {today}");
+Console.WriteLine(value: $"Today just date: {today.Date}");
+Console.WriteLine(value: $"Birthday full format: {birthday}");
