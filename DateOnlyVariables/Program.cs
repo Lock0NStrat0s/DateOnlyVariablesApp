5b0d8890de0bf45433e8cbcdc2ec7b50ae45fca9
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-09 00:09:40 -0600

    code complete.

--- DateOnlyVariables/Program.cs
+++ DateOnlyVariables/Program.cs
@@ -5,6 +5,6 @@ DateOnly birthday = DateOnly.Parse(s: "6/11/1998");
 
 Console.WriteLine(birthday.ToString(format: "MMMM dd, yyyy"));
 
-Console.WriteLine(value: $"Today full format: {today}");
-Console.WriteLine(value: $"Today just date: {today.Date}");
-Console.WriteLine(value: $"Birthday full format: {birthday}");
+Console.WriteLine(value: $"Today full format: {today}");        //DateTime full
+Console.WriteLine(value: $"Today just date: {today.Date}");     //DateTime "date only" (has time set to midnight)
+Console.WriteLine(value: $"Birthday full format: {birthday}");  //Date Only
